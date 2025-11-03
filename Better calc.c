01c4cdab22202455ccc84efb6ee23bc669c65f99
Zaid-k1 <zaidkeylany@gmail.com>
2025-11-03 19:00:05 +0300

    Added two more functions and looped the whole thing with while loop.

--- Better calc.c
+++ Better calc.c
@@ -1,15 +1,23 @@
 #include<stdio.h>
 #include<stdlib.h>
+#include <math.h>
+
 int main() {
 	
 	double num2;
 	double num1;
+	double num0;
 	char func;
+	int zaid = 6;
 	
-	printf("Enter first number: ");
-	scanf("%lf", &num1);                                       //When we use scanf for doubles we use %lf
+	
+	printf("Functions available for now: +, -, *, /, ^, v(for square root)\n");
+	while(zaid == 6) {
 	printf("Enter a function: ");
-	scanf(" %c", &func);                                       //When we use scanf for char we put a space first
+	scanf(" %c", &func);										//When we use scanf for char we put a space first
+	if (func != 'v') {										
+	printf("Enter first number: ");
+	scanf("%lf", &num1);                                       //When we use scanf for doubles we use %lf                                 
 	printf("Enter second number: ");
 	scanf("%lf", &num2);
 	
@@ -17,25 +25,40 @@ int main() {
 	double sub = num1 - num2;
 	double mult = num1 * num2;
 	double div = num1 / num2;
+	double power = pow(num1, num2);
 	
 	switch (func) {
 		
 		case '+' :
-			printf("%f", sum);
+			printf("%.2f + %.2f = %.3f\n", num1, num2, sum);
 			break;
 		case '-' :
-			printf("%f", sub);
+			printf("%.2f - %.2f = %.3f\n", num1, num2, sub);
 			break;
 		case '*' :
-			printf("%f", mult);
+			printf("%.2f * %.2f = %.3f\n", num1, num2, mult);
 			break;
 		case '/' :
-			printf ("%f", div);
+			printf("%.2f / %.2f = %.3f\n", num1, num2, div);
+			break;
+		case '^' :
+			printf("%.2f ^ %.2f = %.3f\n", num1, num2, power);
 			break;
 		default :
-			printf ("Invalid function,\n");
-			printf ("Please enter one of the (+, -, * or /) functions.");
-	
+			printf ("INVALID FUNCTION\n");
+			printf("Functions available for now: +, -, *, /, ^, v(for square root)\n");
+	}
+	}
+	else if (func == 'v') {
+		printf("Enter a number: ");
+		scanf("%lf", &num0);
+		double squareRoot = sqrt(num0);
+		printf("%f\n", squareRoot);
+	}
+	else {
+		printf ("INVALID FUNCTION\n");
+		printf("Functions available for now: +, -, *, /, ^, v(for square root)\n");
+	}
 	}
 	return 0;
 	
