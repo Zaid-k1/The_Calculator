6c11b654f89153b0c6d8e0776cde417dd2cc4317
Zaid-k1 <zaidkeylany@gmail.com>
2025-11-22 02:20:33 +0300

    Major changes + added 2 funcs

--- Better calc.c
+++ Better calc.c
@@ -3,62 +3,87 @@
 #include <math.h>
 
 int main() {
-	
-	double num2;
-	double num1;
-	double num0;
+	double num0, num1, num2;
 	char func;
+	int cb=1;
 	
-	printf("Functions available for now: +, -, *, /, ^, v(for square root)\n");
-	while(1) {
+	printf("Functions available for now: +, -, *, /, ^, v(square root), |(Absolute value), !\n");
+	printf("Enter 'END' to stop.\n");
+	
+	while(cb!=0) {
+	printf("\n");
 	printf("Enter a function: ");
 	scanf(" %c", &func);										//When we use scanf for char we put a space first
 	
-	if (func != 'v') {										
-	printf("Enter first number: ");
-	scanf("%lf", &num1);                                       //When we use scanf for doubles we use %lf                                 
-	printf("Enter second number: ");
-	scanf("%lf", &num2);
-	
-	double sum = num1 + num2;
-	double sub = num1 - num2;
-	double mult = num1 * num2;
-	double div = num1 / num2;
-	double power = pow(num1, num2);
-	
-	switch (func) {
-		case '+' :
-			printf("%.2f + %.2f = %.3f\n", num1, num2, sum);
-			break;
-		case '-' :
-			printf("%.2f - %.2f = %.3f\n", num1, num2, sub);
-			break;
-		case '*' :
-			printf("%.2f * %.2f = %.3f\n", num1, num2, mult);
-			break;
-		case '/' :
-			printf("%.2f / %.2f = %.3f\n", num1, num2, div);
-			break;
-		case '^' :
-			printf("%.2f ^ %.2f = %.3f\n", num1, num2, power);
-			break;
-		default :
-			printf ("INVALID FUNCTION\n");
-			printf("Functions available for now: +, -, *, /, ^, v(for square root)\n");
-		}
+	if(func=='E'){
+		cb=0;
 	}
-	else if (func == 'v') {
+	
+	else if(func=='+'||func=='-'||func=='*'||func=='/')
+	{										
+		printf("Enter first number: ");
+		scanf("%lf", &num1);                                       //When we use scanf for doubles we use %lf                                 
+		printf("Enter second number: ");
+		scanf("%lf", &num2);
+	
+		double sum = num1 + num2;
+		double sub = num1 - num2;
+		double mult = num1 * num2;
+		double div = num1 / num2;
+		double power = pow(num1, num2);
+	
+		switch (func) {
+			case '+' :
+				printf("%.2f + %.2f = %.3f\n", num1, num2, sum);
+				break;
+			case '-' :
+				printf("%.2f - %.2f = %.3f\n", num1, num2, sub);
+				break;
+			case '*' :
+				printf("%.2f * %.2f = %.3f\n", num1, num2, mult);
+				break;
+			case '/' :
+				printf("%.2f / %.2f = %.3f\n", num1, num2, div);
+				break;
+			case '^' :
+				printf("%.2f ^ %.2f = %.3f\n", num1, num2, power);
+				break;
+			default :
+				printf ("INVALID FUNCTION\n");
+				printf("Functions available for now: +, -, *, /, ^, v(for square root)\n");
+			}
+	}
+	
+	else {
 		printf("Enter a number: ");
 		scanf("%lf", &num0);
+		int i, num0fac=num0;
 		double squareRoot = sqrt(num0);
-		printf("%f\n", squareRoot);
-	}
-	else {
-		printf ("INVALID FUNCTION\n");
-		printf("Functions available for now: +, -, *, /, ^, v(for square root)\n");
-	}
+		double absValue;
+			switch(func){
+				case'v':
+					printf("%f\n", squareRoot);
+					break;
+				case'|':
+					if(num0>=0) absValue=num0;
+					else absValue=-num0;
+					printf("|%.3f|=%.3f\n", num0, absValue);
+					break;
+				case'!':
+					for(i=num0-1;i>=1;i--){
+						num0fac=num0fac*i;
+					}
+					printf("%.0f!=%d",num0,num0fac);
+					break;
+				default:
+					printf ("INVALID FUNCTION\n");
+					printf("Functions available for now: +, -, *, /, ^, v(for square root)\n");	
+			}
+
+		}
 	
 	}
+
 	return 0;
 	
 }
